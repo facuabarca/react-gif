599f1be23e4295fb33c893c7fef7a673362c0bdf
facuabarca <facuabarca@gmail.com>
2022-12-28 13:10:32 +0100

    feat(gif): first commit

---
+++ src/GiftApp.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+import {  AddCategory, GifGrid} from './components'
+
+export const GiftApp = () => {
+
+    const [categories, setCategories] = useState([ 'One Punch' ])
+
+    const onAddCategory = (newCategory) => {
+
+        if (categories.includes(newCategory)) return
+
+        setCategories(cat => [ newCategory, ...cat ])
+    }
+
+    return (
+        <>
+            <h1>GifExpertApp</h1>
+
+            <AddCategory 
+              
+                onNewCategory={ onAddCategory }
+            />
+
+            {   
+                categories.map( cat => 
+                    (
+                        <GifGrid key={ cat } category={ cat } />
+                    )
+                )
+            }
+        </>
+    )
+}
